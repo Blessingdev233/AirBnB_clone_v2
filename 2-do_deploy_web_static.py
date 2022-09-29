8345b6a3d3f401d813791579edbcf709b6ec407f
Blessing Tetteh Lartey <82586772+Blessingdev233@users.noreply.github.com>
2022-09-29 20:59:37 +0000

    Server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['34.138.32.248', '3.226.74.205']
+env.hosts = ['34.231.70.64', '34.232.63.200']
 
 
 def do_deploy(archive_path):
